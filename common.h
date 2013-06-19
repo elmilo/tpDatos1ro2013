78ca2ddd173c2415f37a2f39a9ae7e6a52f5a47b
erandros <guidotarsia@hotmail.com>
2013-06-19 18:00:02 -0300

    Cambios por la mitad

--- common.h
+++ common.h
@@ -19,6 +19,7 @@ typedef unsigned int tOffset;
 
 const unsigned int MAX_DIC_SIZE = 1024;
 const unsigned int BITS_PER_BYTE = 8;
+const unsigned int FRAG_SIZE = 128;
 const std::string TEMPDIR;
 const std::string OUTDIR;
 const unsigned int maxTamanioBloque = 182379793; //elegir uno o definir en momento de ejecucion
